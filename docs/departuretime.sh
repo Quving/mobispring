ed935a1c67016521cd24ffc8472c19384611c005
vngu <vinh-ngu@hotmail.com>
2018-01-02 17:10:37 +0100

    Update example post.

--- docs/departuretime.sh
+++ docs/departuretime.sh
@@ -1,1 +1,2 @@
-curl -X POST -H "Content-Type: application/json" -d "{'station':'Farmsen', 'hhMMyyyy':'24.12.2017', 'HHmm':'23:24', maxList:'2'}" http://localhost:8080/api/geofox/departuretime
+curl -X POST -H "Content-Type: application/json" -d '{"station":"Farmsen", "hhMMyyyy":"03.01.2018", "HHmm":"23:24", "maxList":"2"}' http://localhost:8080/api/geofox/departuretime/
+
